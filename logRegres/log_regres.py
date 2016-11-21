3b046554f9166ace1a354f0512ddab1d758dc83d
延昊南 <yhnkb@hotmail.com>
2016-11-21 22:49:44 +0800

    5.3 示例：从疝气病症预测病马的死亡率

--- logRegres/log_regres.py
+++ logRegres/log_regres.py
@@ -89,3 +89,44 @@ def stoc_grad_ascent1(data_matrix, class_labels, num_iter=150):
             weights = weights + alpha * error * data_matrix[rand_index]
             del(data_index[rand_index])
     return weights
+
+
+def classify_vector(in_x, weights):
+    prob = sigmoid(sum(in_x * weights))
+    return 1.0 if prob > 0.5 else 0.0
+
+
+def colic_test():
+    fr_train = open(material_path + 'horseColicTraining.txt')
+    fr_test = open(material_path + 'horseColicTest.txt')
+    training_set = []
+    training_labels = []
+    for line in fr_train.readlines():
+        curr_line = line.strip().split('\t')
+        line_arr = []
+        for i in range(21):
+            line_arr.append(float(curr_line[i]))
+        training_set.append(line_arr)
+        training_labels.append(float(curr_line[21]))
+    train_weights = stoc_grad_ascent1(array(training_set), training_labels, 500)
+    error_count = 0
+    num_test_vec = 0.0
+    for line in fr_test.readlines():
+        num_test_vec += 1.0
+        curr_line = line.strip().split('\t')
+        line_arr = []
+        for i in range(21):
+            line_arr.append(float(curr_line[i]))
+        if int(classify_vector(array(line_arr), train_weights)) != int(curr_line[21]):
+            error_count += 1
+    error_rate = (float(error_count) / num_test_vec)
+    print('the error rate of this test is: %f' % error_rate)
+    return error_rate
+
+
+def multi_test():
+    num_tests = 10
+    error_sum = 0.0
+    for k in range(num_tests):
+        error_sum += colic_test()
+    print('after %d iterations the average error rate is: %f' % (num_tests, error_sum / float(num_tests)))
